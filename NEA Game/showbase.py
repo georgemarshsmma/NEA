b73f70bb3636ee3de80b36385005899b41945780
georgemarshsmma <148215719+georgemarshsmma@users.noreply.github.com>
2023-11-06 14:01:03 +0000

    Update showbase.py

--- NEA Game/showbase.py
+++ NEA Game/showbase.py
@@ -8,10 +8,9 @@ class WashingtonBullets(ShowBase):
         props = WindowProperties() #creates window properties
         props.setTitle('Washington Bullets') #name of app running
         props.setSize(1280, 720) #sets display size
-        props.setCursorHidden(True)
-
+        props.setCursorHidden(True) #hides cursor
         base.win.requestProperties(props)
-
+        self.camera.setP(0) #sets the pitch (tilt) of camera
 
 app = WashingtonBullets()
 app.run()
